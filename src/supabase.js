5986453eb2a14e7973526fecd7ec6e3cec17ac93
sappirsap-cpu <sappirsap@gmail.com>
2026-04-23 23:36:58 +0300

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,14 @@
+// src/supabase.js
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = 'https://zgwacddfwhrdeckmszdm.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inpnd2FjZGRmd2hyZGVja21zemRtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY1NzQ1NTYsImV4cCI6MjA5MjE1MDU1Nn0.Ykad5hC0UcHozcW1HMHzAhWrWHeNGKUmyq67U_gbsm0';
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    persistSession: true,
+    autoRefreshToken: true,
+    detectSessionInUrl: true,
+    storage: window.localStorage,
+  },
+});
